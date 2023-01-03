9dc50e74e2f9fc24db377523f2f47a36e313d432
marcos-mv <vinicius_kof36@hotmail.com>
2023-01-02 21:54:53 -0300

    Adding function random points to the grid.
    Co-authored-by: brunocmo <brunocmo.nunes@gmail.com>

--- points.py
+++ points.py
@@ -1,2 +1,19 @@
-points = [(20, 43), (21, 21), (28, 39), (2, 10), (11, 47),
-          (35, 46), (36, 3), (44, 41), (13, 5), (40, 19), (45,45)]
+from random import randint 
+
+#Para gerar os pontos de maneira randomica basta executar essa função
+#caso queira testar com seus proprios pontos basta comentar essa primeira função e descomentar a segunda função.
+
+def rand_cluster(n):
+    x = 0
+    y = 0
+    points = []
+    for i in range(n):
+        x = randint(0,50) 
+        y = randint(0,50) 
+        points.append((x, y))
+    return points
+
+# def rand_cluster(n):
+#     points = [(20, 43), (21, 21), (28, 39), (2, 10), (11, 47),
+#           (35, 46), (36, 3), (44, 41), (13, 5), (40, 19), (45,45)]
+#     return points
