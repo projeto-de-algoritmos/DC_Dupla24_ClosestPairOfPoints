4d4d8f815d0a7b6e0936068fdbf0b8148f27eab2
marcos-mv <vinicius_kof36@hotmail.com>
2023-01-02 23:13:53 -0300

    Adding draw functions to the project.
    Co-authored-by: brunocmo <brunocmo.nunes@gmail.com>

--- main.py
+++ main.py
@@ -25,4 +25,36 @@ points = []
 
 points = rand_points(10)
 
+def draw_grid(l):
+    GAMEWINDOW.fill(orange)
+    a = 0
+    b = 0
+    c = 0
+    d = 800
+    for i in range(50):
+        pygame.draw.line(GAMEWINDOW, grey, (a, b), (c, d), 1)
+        a += 16
+        c += 16
 
+    a = 0
+    b = 0
+    c = 800
+    d = 0
+    for i in range(50):
+        pygame.draw.line(GAMEWINDOW, grey, (a, b), (c, d), 1)
+        b += 16
+        d += 16
+
+    for i in l:
+        pygame.draw.rect(
+            GAMEWINDOW, red, (i[0]*16, abs((49 - i[1])*16), 16, 16))
+
+def draw_line(a, b, color):
+
+    x1 = a[0]
+    y1 = a[1]
+    x2 = b[0]
+    y2 = b[1]
+    y1 = abs(50 - y1)
+    y2 = abs(50 - y2)
+    pygame.draw.line(GAMEWINDOW, color, (x1*16, y1*16), (x2*16, y2*16), 5)
